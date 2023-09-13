4a4f1a382e4929fb84a6f3c3f8f1772b56e55308
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-13 17:29:22 +0200

    [ADD] Click button up & down events

--- js/script.js
+++ js/script.js
@@ -5,11 +5,31 @@ const btnDown = document.querySelector(".btn-down");
 const images = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"]
 
 for(i = 0; i < 5; i++){
-
   console.log(images[i]);
 
   const img = images[i];
-  itemsWrapper.innerHTML += `<img src = "${img}" class ="hide">`
+  itemsWrapper.innerHTML += `<img src = "${img}" class ="items hide">`
 }
+
+const itemsCollection = document.getElementsByClassName("items");
+
+let counterImg = 0;
+itemsCollection[counterImg].classList.remove("hide");
+
+btnUp.addEventListener("click", function(){
+  itemsCollection[counterImg].classList.add("hide");
+
+  counterImg ++;
+
+  itemsCollection[counterImg].classList.remove("hide");
+}) 
+
+btnDown.addEventListener("click", function(){
+  itemsCollection[counterImg].classList.add("hide");
+
+  counterImg --;
+
+  itemsCollection[counterImg ].classList.remove("hide");
+}) 
 
 
