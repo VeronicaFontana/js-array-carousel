01e612c8d514079cbefa5d1f8280cfb225f373fd
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-14 08:33:03 +0200

    [FIX] infinite cycle

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const btnDown = document.querySelector(".btn-down");
 
 const images = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"]
 
+images.push(" ");
 
-for(i = 0; i < 5; i++){
+
+for(i = 0; i < 6; i++){
   console.log(images[i]);
 
   const img = images[i];
@@ -34,21 +36,20 @@ btnUp.addEventListener("click", function(){
 }) 
 
 btnDown.addEventListener("click", function(){
-  itemsCollection[counterImg].classList.add("hide");
-
-  counterImg--;
-
-  itemsCollection[counterImg].classList.remove("hide");
-
   if(counterImg === 0){
     itemsCollection[counterImg].classList.add("hide");
 
-    counterImg = (itemsCollection.length - 1);
+    counterImg = (itemsCollection.length - 2);
+
+    itemsCollection[counterImg].classList.remove("hide");
+  }else{
+    itemsCollection[counterImg].classList.add("hide");
+
+    counterImg--;
 
     itemsCollection[counterImg].classList.remove("hide");
   }
 }) 
-
 
 
 
